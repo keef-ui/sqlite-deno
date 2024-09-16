184cd33ae264dad47d57360077ffe2528c251e62
Keerthi v <keef-vp@hotmail.com>
2024-09-16 22:15:08 +0100

    add main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,3 @@ let name = 'Breaking The Rules'
 let results = await database.sql`SELECT * FROM tracks WHERE name = ${name}`
 // => returns [{ AlbumId: 1, Name: 'Breaking The Rules', Composer: 'Angus Young... }]
 console.log(results)
-
-
-
