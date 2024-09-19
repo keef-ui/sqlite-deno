d5132c22eda76c29b63b387d71c5d2a3e83e0557
keef-v <keef-vp@hotmail.com>
2024-09-19 11:56:04 +0100

    cleaned up and refactored - dleteted unwantes stuff

--- mapform/testupload/testserver.ts
+++ mapform/testupload/testserver.ts
@@ -59,6 +59,54 @@ router.post("/upload", async (context) => {
   }
 });
 
+
+//incident form 
+router.post("/incident", async (context) => {
+  try {
+    const body = context.request.body({ type: "form-data" });
+    const formData = await body.value.read({ maxSize: 10 * 1024 * 1024 }); // 10 MB limit
+
+    const file = formData.files?.[0];
+    console.log(file.originalName)
+    if (!file) {
+      context.response.status = 400;
+      context.response.body = { message: "No file uploaded" };
+      return;
+    }
+
+    console.log(formData.fields.latitude)
+    // Ensure the uploads directory exists
+    await Deno.mkdir("uploads", { recursive: true });
+
+ 
+    const filePath = `uploads/${file.originalName}`;
+
+
+
+    // Check if file.content is available
+    if (!file.content) {
+      throw new Error("File content is missing");
+    }
+
+
+
+    // Write the file to the uploads directory
+    await Deno.writeFile(filePath, file.content);
+
+    context.response.body = { 
+      message: "File uploaded successfully", 
+      filename: file.originalName,
+      originalFilename: file.filename
+    };
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    context.response.status = 500;
+    context.response.body = { message: `Error uploading file: ${error.message}` };
+  }
+});
+
+
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
