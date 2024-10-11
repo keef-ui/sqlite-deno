4aaba36667bd2e713c83fb124afc08631d7c91d2
keef-v <keef-vp@hotmail.com>
2024-10-11 14:40:15 +0100

    incident-app - fixed image upload to clodinary

--- incidents-app/jwttest.ts
+++ incidents-app/jwttest.ts
@@ -5,9 +5,21 @@ import {
 } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { create, verify } from "https://deno.land/x/djwt/mod.ts";
 import { Handlebars } from "https://deno.land/x/handlebars@v0.9.0/mod.ts";
+import { v2 as cloudinary } from "npm:cloudinary";
+import { uploadPromiseDenoCloudinary } from "../cloudinary/uploadPromiseDenoCloudinary.ts";
 // import { Model } from "../db/orm.ts";
 import { Model } from "../db/orm-buggy.ts";
 import "jsr:@std/dotenv/load";
+
+const  cl_name=Deno.env.get("CLOUDINARY_NAME")
+const cl_key=Deno.env.get("CLOUDINARY_KEY")
+const cl_secret=Deno.env.get("CLOUDINARY_SECRET")
+// Cloudinary configuration
+cloudinary.config({ 
+  cloud_name:cl_name , 
+  api_key: cl_key , 
+  api_secret:cl_secret  // Click 'View API Keys' above to copy your API secret
+});
 
 //Web server
 const app = new Application();
@@ -189,6 +201,65 @@ router.get("/api/incidents", async (ctx) => {
   // ctx.response.body = result.rows;
   ctx.response.body = allIncidents;
 });
+
+// Incident form
+router.post("/api/incident", async (ctx) => {
+  console.log("post /api/incident....");
+  try {
+    const body = ctx.request.body({ type: "form-data" });
+    const formData = await body.value.read({ maxSize: 10 * 1024 * 1024 }); // 10 MB limit
+
+    const file = formData.files?.[0];
+   
+    if (!file) {
+      ctx.response.status = 400;
+      ctx.response.body = { message: "No file uploaded" };
+      return;
+    }
+
+    console.log("fields: ", formData.fields);
+    console.log("file name: " + file.originalName);
+
+    //Upload file to Cloudinary
+    const uploadResult = await uploadPromiseDenoCloudinary(file);
+
+    // DB Insertion of incident form
+    await Model.initialize(db_connectionString);
+
+    class Incident extends Model {
+      static tableName = db_table;
+    }
+
+
+    // const uploadResultsecure_url = file.originalFilename
+    // Insert the form data into the database
+    await Incident.insert({
+      email: formData.fields.email,
+      description: formData.fields.description || "No description provided",
+      latitude: formData.fields.latitude,
+      longitude: formData.fields.longitude,
+      address: formData.fields.address || "No address provided",
+      image: uploadResult.secure_url,
+    });
+
+    // Return success response
+    ctx.response.body = {
+      message: "File uploaded successfully",
+      filename: file.originalName,
+      cloudinaryUrl: uploadResult.secure_url,
+      latitude: formData.fields.latitude,
+      longitude: formData.fields.longitude,
+      success: true,
+    };
+    Model.database.close();
+  } catch (error) {
+    console.error("Error:", error);
+    ctx.response.status = 500;
+    ctx.response.body = {
+      message: `Oops... something went wrong: ${error.message}`,
+    };
+  }
+})
 
 app.use(router.routes());
 app.use(router.allowedMethods());
